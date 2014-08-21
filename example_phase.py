47011862951c48c072542e1fcc676701c4557244
JoeOG <Joe@wolf4181maclt.materials.ox.ac.uk>
2014-08-21 14:51:53 +0100

    update example_phase
    example_phase updated to provide example of run used in paper

--- example_phase.py
+++ example_phase.py
@@ -5,22 +5,17 @@ import os
 import imp
 import load_errors
 
-home = os.environ['HOME']
-sp = imp.load_source('sp','%s/perfect_matching-master-3/sp.py'%home)
-
 random.seed(0)
 
-try:
-    size, errortype, tSteps, sd, output_file = sys.argv[1:]
-    size = int(size)
-    errortype=str(errortype)
-    tSteps=int(tSteps)
-    sd = float(sd)
-except:
-    print "Usage: [size] [errortype] [tSteps] [sdPlane] [output_file] "
-    sys.exit(-1)
 
-#run3Dspin(size,errortype,tsteps,[sdInX,sdInY,sdInZ,jitter,pX,pY,pZ,prep,meas,data],[time,space],edge)
+#run3Dspin(size,errortype,orbit,tsteps,[sdInX,sdInY,sdInZ,jitter,pX,pY,pZ,prep,meas,data],[time,space],edge)
+
+
+size=10
+errortype='normal'
+orbit='circle'
+tSteps=20
+sd=0.1
 
 X,Z,Q = 0,0,0
 
@@ -29,14 +24,16 @@ timeweight=1400
 boundary=1200
 
 for i in range(n_trials):
-    [x,z] = sp.run3Dspin(size,errortype,tSteps,[sd,sd,0.5*sd,0.0004,0.001/3,0.001/3,0.001/3,0.01,0.05,0.002],[1000,timeweight],boundary)
+    [x,z] = sp.run3Dspin(size,errortype,orbit, tSteps,[sd,sd,0.5*sd,0.0004,0.001/3,0.001/3,0.001/3,0.01,0.05,0.002],[1000,timeweight],boundary)
 
     X+= 1 if x==1 else 0
     Z+= 1 if z==1 else 0
     Q += 1 if x == 1 and z ==1 else 0
 
 
-f = open(output_file, 'a')
-f.write("%d %f %d %d %d %d| %d %d %d\n"%(size, sd, n_trials, tSteps,timeweight,boundary, X, Z, Q))
-f.close()
+print 'lattice size: ',size
+print 'probe orbit: ',orbit
+print 'error distribution',errortype
+print 'sd in plane',sd
+print X,' /',Z,'/',n_trials,'were successfully decoded: ',Q
 
