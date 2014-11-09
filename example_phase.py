870e3f6fd7be2f9064735f78531636e6810c8ab0
Naomi Nickerson <naominickerson@gmail.com>
2014-11-09 16:25:16 +0000

    update orbital probe example

--- example_phase.py
+++ example_phase.py
@@ -11,21 +11,36 @@ random.seed(0)
 #run3Dspin(size,errortype,orbit,tsteps,[sdInX,sdInY,sdInZ,jitter,pX,pY,pZ,prep,meas,data],[time,space],edge)
 
 
-size=10
+size=8
 errortype='normal'
 orbit='circle'
 tSteps=20
 sd=0.1
 
+sdInX = sd
+sdInY= sd
+sdInZ=sd*0.5
+jitter = 0.0004
+pX=0.001/3
+pY=0.01/3
+pZ=0.01/3
+prep = 0.01
+meas = 0.05
+data = 0.002
+
 X,Z,Q = 0,0,0
 
-n_trials=1500
+n_trials=10
 spaceweight=1400
 boundary=1200
 
-for i in range(n_trials):
-    [x,z] = sp.run3Dspin(size,errortype,orbit, tSteps,[sd,sd,0.5*sd,0.0004,0.001/3,0.001/3,0.001/3,0.01,0.05,0.002],[1000,spaceweight],boundary)
+corrected = 0
 
+for i in range(n_trials):
+
+    [x,z] = sp.run3Dspin(size,errortype,orbit, tSteps,[sdInX,sdInY,sdInZ,jitter,pX,pY,pZ,prep,meas,data],[1000,spaceweight],boundary)
+
+    corrected+=1 if [x,z]==[1,1] else 0
     X+= 1 if x==1 else 0
     Z+= 1 if z==1 else 0
     Q += 1 if x == 1 and z ==1 else 0
@@ -35,5 +50,5 @@ print 'lattice size: ',size
 print 'probe orbit: ',orbit
 print 'error distribution',errortype
 print 'sd in plane',sd
-print X,' /',Z,'/',n_trials,'were successfully decoded: ',Q
+print corrected,'/',n_trials,'were successfully decoded: '
 
